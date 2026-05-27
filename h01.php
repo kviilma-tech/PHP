0c2da0846f85c372a9a7f63a8019034a0667df79
kviilma-tech <kviilma@hkhk.edu.ee>
2026-05-27 21:02:15 +0300

    first commit

---
+++ h01.php
@@ -0,0 +1,33 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+/*
+    01 - PHP - Muutujad
+    Kaspar Viilma
+    27.05.2026
+*/
+
+// Muutuja loomine
+        $eesNimi = "Kaspar";
+        $perekonnaNimi = "Viilma";
+        $nimi = $eesNimi . " " . $perekonnaNimi;
+        $sünniaasta = 2009;
+        $tähtkuju = "Kala";
+
+        echo $nimi
+        echo $sünniaasta
+        echo $tähtkuju
+        echo '"It\s My Life" - Dr.Alban<br>';
+
+?>
+
